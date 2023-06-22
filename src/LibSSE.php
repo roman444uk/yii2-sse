cda83947977fb947b580eafaf5b1ebe41cd66a58
roman <roman444uk@mail.ru>
2023-06-22 18:12:20 -0400

    Initial commit

---
+++ src/LibSSE.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author Danny Carrillo <odannycx@gmail.com>
+ * @package yii2-sse
+ */
+namespace odannyc\Yii2SSE;
+use Sse\SSE;
+
+/**
+ * Main wrapper for the LibSSE-php package.
+ */
+class LibSSE extends SSE
+{
+    //
+}
